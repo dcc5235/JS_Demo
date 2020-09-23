a6c5d433cc31549ef6de767588685ac8d4b222c3
Dany <dany@Danys-MacBook-Pro.local>
2020-09-23 15:14:27 -0500

    nesting conditions

--- app.js
+++ app.js
@@ -68,3 +68,14 @@
 // }
 
 //////////////////////////nesting conditionals
+let password = "hellokitty";
+
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    console.log("Valid password!");
+  } else {
+    console.log("Password is long enough, but cannot contain spaces");
+  }
+} else {
+  console.log("Password must be longer");
+}
