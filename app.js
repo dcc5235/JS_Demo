e601e588e2e996bf51861e1963071c1dfd8c4a47
Dany <dany@Danys-MacBook-Pro.local>
2020-09-23 13:50:11 -0500

    else if examples

--- app.js
+++ app.js
@@ -15,12 +15,29 @@
 // }
 
 //////////////////////////checking for a single condition
-let rating = 3;
-if (rating === 3) {
-  console.log("You are a superstar!");
-}
+// let rating = 3;
+// if (rating === 3) {
+//   console.log("You are a superstar!");
+// }
 
 // let rating = 2;
 // if (rating < 3) {
 //   console.log("You could use some improvement...");
 // }
+
+// let num = 37;
+
+// if (num % 2 !== 0) {
+//   console.log("odd number");
+// }
+
+//////////////////////////else if
+let rating = 3;
+
+if (rating === 3) {
+  console.log("You are a superstar!");
+} else if (rating === 2) {
+  console.log("Meets expectations");
+} else if (rating === 1) {
+  console.log("needs improvement");
+}
