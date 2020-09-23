0ed4f5464f2465106e1e9eb3d2e39d0c80fe8afc
Dany <dany@Danys-MacBook-Pro.local>
2020-09-23 13:38:12 -0500

    adding if statements

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 // alert("It's Working!");
 
-console.log(3 + 4);
-console.error("OH NOO!!!");
-console.log(3 + 4, "hello", true);
+//////////////////////////consol.log
+// console.log(3 + 4);
+// console.error("OH NOO!!!");
+// console.log(3 + 4, "hello", true);
+
+//////////////////////////if statements
+// if (1 === 1) {
+//   console.log("It's True!");
+// }
+
+// if (2 !== 2) {
+//   console.log("It's True!");
+// }
+
+//////////////////////////checking for a single condition
+let rating = 3;
+if (rating === 3) {
+  console.log("You are a superstar!");
+}
+
+// let rating = 2;
+// if (rating < 3) {
+//   console.log("You could use some improvement...");
+// }
