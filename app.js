4815ef0e872669e5dcb8292ef683fb7bd7635c47
Dany <dany@Danys-MacBook-Pro.local>
2020-09-23 20:18:58 -0500

    && logical operators

--- app.js
+++ app.js
@@ -15,3 +15,20 @@
 // } else {
 //   console.log("please log in!");
 // }
+
+//////////////////////////logical operators
+// let password = "chickenGal";
+
+// if (password.length >= 8 && password.indexOf(" ") === -1) {
+//   console.log(`valid password`);
+// } else {
+//   console.log(`invalid password`);
+// }
+
+let num = 34;
+
+if (num >= 1 && num <= 10) {
+  console.log(`number is between 1 and 10`);
+} else {
+  console.log(`please guess a number between 1 and 10`);
+}
