12e4feccb70b2735eee0f249dec561e4bfe8c97c
Dany <dany@Danys-MacBook-Pro.local>
2020-09-24 12:26:48 -0500

    array indices

--- app.js
+++ app.js
@@ -2,3 +2,6 @@
 let shoppingList = ["cereal", "cheese", "ice"];
 
 let myCollection = [12, "dog", true, null, NaN];
+
+//////////////////////////array indices
+let colors = ["red", "orange", "yellow", "green"];
