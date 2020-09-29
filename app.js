dd96bd8a85a8624ab727da136a021b0a7e1787c8
Dany <dany@Danys-MacBook-Pro.local>
2020-09-29 16:31:07 -0500

    for loops examples

--- app.js
+++ app.js
@@ -1,99 +1,123 @@
-//////////////////////////objects
+//for loops
 
-// Creating Object Literals
-const fitBitData = {
-  totalSteps: 308727,
-  totalMiles: 211.7,
-  avgCalorieBurn: 5755,
-  workoutsThisWeek: '5 of 7',
-  avgGoodSleep: '2:13'
-};
-console.log(`fitBitData example \n`, fitBitData)
+for ([initialExpression]; [condition]; [incrementExpression])
+  // Example 1
+  for (let i = 1; i <= 10; i++) {
+    console.log("Hello");
+  }
 
-fitBitData example 
- {
-  totalSteps: 308727,
-  totalMiles: 211.7,
-  avgCalorieBurn: 5755,
-  workoutsThisWeek: '5 of 7',
-  avgGoodSleep: '2:13'
- }
+//i variable looping Hello +1 until the value is equal to or less than 10
+Hello;
+Hello;
+Hello;
+Hello;
+Hello;
+Hello;
+Hello;
+Hello;
+Hello;
+Hello;
 
-console.log(`totalMiles returned \n`, fitBitData.totalMiles)
 
-totalMiles returned 
- 211.7
+//loops and arrays
+// Example 1
+const examScores = [98, 77, 84, 91, 57, 66];
 
-console.log(`totalMiles returned \n`, fitBitData.totalMiles)
-
-totalMiles returned 
- 211.7
-
- // Accessing Object Properties
- const numbers = {
-  100: 'one hundred',
-  16: 'sixteen'
-  };
-  
-  console.log(numbers[100]) 
-  "one hundred"
-
-// Adding and Updating Properties
-const userReviews = {};
-
-userReviews['queenBee49'] = 4;
-userReviews.mrSmith78 = 3.5;
-console.log('Added properties for queenBee49 & mrSmith78 \n', userReviews)
-
-Added properties for queenBee49 & mrSmith78 
- { queenBee49: 4, mrSmith78: 3.5 }
-
-userReviews['queenBee49'] += 2;
-console.log(`Added 2 to queenBee49 value \n`,  userReviews)
-
-Added 2 to queenBee49 value 
- { queenBee49: 6, mrSmith78: 3.5 }
-
-// Nested Arrays and Objects
-const student = {
-  firstName: 'Dany',
-  lastName: 'Chheang',
-  strengths: ['English', 'Art'],
-exams: {
-  midterm: 92,
-  final: 88
-}
-};
-
-console.log(`Value of avg of midterm and final \n`,(student.exams.midterm + student.exams.final)/2)
-
-Value of avg of midterm and final 
- 90
-
-//Objects and Reference Types
-const palette = {
-  red: '#eb4d4b',
-  yellow: '#f9ca24',
-  blue: '#30336b'
-};
-
-const palette2 = palette;
-palette2.green = '#ebf876';
-console.log(`Example displays all objects, including green \n`,palette2)
-
-Example displays all objects, including green 
- {
-  red: '#eb4d4b',
-  yellow: '#f9ca24',
-  blue: '#30336b',
-  green: '#ebf876'
+for(let i = 0; i < examScores.length; i++){
+  console.log(i, examScores[i]);
 }
 
-//Array/Object Equality
+0 98
+1 77
+2 84
+3 91
+4 57
+5 66
 
-let nums = [1, 2, 3];
-let mystery = [1, 2, 3];
-let moreNums = nums;
-console.log(moreNums);
+// Example 2
+const word = 'stressed';
 
-[ 1, 2, 3 ]
+for (let i = word.length -1; i >=0; i--){
+  console.log(word[i]);
+}
+
+//nested for loops
+```jsx
+for(let i = 1; i <= 10; i++) {
+  console.log('outer loop:', i);
+  for(let j = 10; j >= 0; j -= 2){
+    console.log('  inner loop', j);
+  }
+}
+
+outer loop: 1
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 2
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 3
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 4
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 5
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 6
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 7
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 8
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 9
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+outer loop: 10
+  inner loop 10
+  inner loop 8
+  inner loop 6
+  inner loop 4
+  inner loop 2
+  inner loop 0
+```
